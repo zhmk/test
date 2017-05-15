f1552c9483e01e23f926fe1a6459d33ed208fab9
zhmk <1349083223@qq.com>
2017-05-15 21:24:50 +0800

    梵蒂冈的房2222

--- app/src/main/java/com/example/videotrainingtest1/myapplication/MainActivity.java
+++ app/src/main/java/com/example/videotrainingtest1/myapplication/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.videotrainingtest1.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
